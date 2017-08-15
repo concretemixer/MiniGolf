6ae0c6539420f05b18839410250d0765693a9561
max <max.pervukhin@gmail.com>
2017-08-16 01:46:46 +0700

    initial

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    enum BallState
+    {
+        Air,
+        Ground,
+        Still
+    }
+
+    BallState state = BallState.Still;
+    float timeInState = 0;
+
+    bool aiming = false;
+    bool ballistic = false;
+
+    private BallState State
+    {
+        get
+        {
+            return state;
+        }
+
+        set
+        {
+            state = value;
+            timeInState = 0;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeInState += Time.deltaTime;
+        if (State == BallState.Still)
+        {
+            if (aiming)
+            {
+                Plane ground = new Plane(Vector3.up, transform.position);
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                float rayDistance;
+                if (ground.Raycast(ray, out rayDistance))
+                {
+                    Vector3 touchGround = ray.origin + ray.direction * rayDistance;
+                    if (Input.GetMouseButton(0))
+                    {
+                        GetComponent<LineRenderer>().enabled = true;
+                        GetComponent<LineRenderer>().SetPosition(0, transform.position);
+                        GetComponent<LineRenderer>().SetPosition(1, touchGround);
+                    }
+
+                    if (Input.GetMouseButtonUp(0))
+                    {
+                        GetComponent<LineRenderer>().enabled = false;
+                        Vector3 dir = touchGround - transform.position;
+
+                        float forceK = Mathf.Clamp(dir.magnitude, 0.5f, 3.0f);
+                        forceK = (forceK - 0.5f) / 2.5f;
+                        dir.Normalize();
+
+                        if (ballistic)
+                        {
+                            Vector3 tangent = Vector3.Cross(Vector3.up, dir);
+
+                            dir = Quaternion.AngleAxis(45, tangent) * dir;
+                            GetComponent<Rigidbody>().AddForce(-dir * Mathf.Lerp(10, 100, forceK), ForceMode.Force);
+                            State = BallState.Air;
+                        }
+                        else
+                        {
+                            GetComponent<Rigidbody>().AddForce(-dir * Mathf.Lerp(10, 100, forceK), ForceMode.Force);
+                            State = BallState.Ground;
+                        }
+
+                        //Debug.Log("d = " + dir.magnitude);
+
+                        aiming = false;
+                        
+                    }
+                }
+            }
+            if (Input.GetMouseButtonDown(0) && !aiming)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 0x100))
+                {
+                    //Vector3 touchBall = ray.origin + ray.direction * hit.distance;
+                    //target.transform.position = touchGround;  
+                    Debug.Log("Touch");
+                    aiming = true;
+                }
+            }
+        }
+        if (State == BallState.Ground)
+        {
+            Vector3 velocity = GetComponent<Rigidbody>().velocity;
+            float distance = velocity.magnitude * Time.deltaTime;
+            Vector3 tangent = Vector3.Cross(Vector3.up,velocity);
+
+            float angle = distance / 0.1f;
+            
+
+            transform.Rotate(tangent, Mathf.Rad2Deg * angle);
+        }
+    }
+
+
+
+
+    void FixedUpdate()
+    {
+        if (State == BallState.Air)
+        {   
+            GetComponent<Rigidbody>().drag = 0;
+            GetComponent<Rigidbody>().AddForce(-Vector3.up * 0.5f, ForceMode.Force);
+        }
+        if (State == BallState.Ground)
+        {
+            if (timeInState > 1.0f && GetComponent<Rigidbody>().velocity.magnitude < 0.01f)
+            {
+                GetComponent<Rigidbody>().velocity = Vector3.zero;
+                State = BallState.Still;
+            }
+        }
+     
+    }
+
+}
---
+++ Assets/Shaders/Refl.shader
@@ -0,0 +1,80 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
+Shader "Custom/Refl"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+		_HeightK("Height K", float) = 0.5
+		_ZeroLevel("Zero Level", float) = 0.0
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Transparent" }
+		LOD 100
+		Blend SrcAlpha OneMinusSrcAlpha
+		Cull Off
+		ZTest Greater
+		ZWrite Off
+		Offset 0,-1
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float4 normal: NORMAL;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{				
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+				fixed4 color : COLOR0;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float _HeightK;
+			float _ZeroLevel;
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				
+				o.vertex = mul(unity_ObjectToWorld, v.vertex);
+				//o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				o.uv.x =  0;
+				o.uv.y = (-(o.vertex.y-_ZeroLevel))*_HeightK;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.color.x = 1;
+				o.color.y = 1;
+				o.color.z = 1;
+				o.color.w = 1;
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, i.uv);
+				// apply fog
+				UNITY_APPLY_FOG(i.fogCoord, col);
+				return col * i.color;
+			}
+			ENDCG
+		}
+	}
+}
